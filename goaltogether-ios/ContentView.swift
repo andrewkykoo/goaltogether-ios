67eda8db92b3c9c9e49f518806379882f0762349
Andrew Koo <andrewkoo@macbookpro.mynetworksettings.com>
2025-02-11 13:50:07 -0500

    initial setup

---
+++ goaltogether-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  goaltogether-ios
+//
+//  Created by Andrew Koo on 2/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ goaltogether-ios/goaltogether_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  goaltogether_iosApp.swift
+//  goaltogether-ios
+//
+//  Created by Andrew Koo on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct goaltogether_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
